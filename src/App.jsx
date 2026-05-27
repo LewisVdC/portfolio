b7d94df231a46eec12aeafba1ca3a7996a1ba746
Lewisvdc <lewis.vandencruyce@leerling.kov.Be>
2026-05-27 07:05:31 +0200

    slight update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
+import Terminal from "./components/terminal";
 
 function App() {
   return (
     <>
       <div id="main">
-        <div id="left" className="half">
-          hi?
-        </div>
-        <div id="right" className="half">
+        <div id="top" className="half">
           hi??
+        </div>
+        <div id="bottom" className="half">
+          <div id="terminal">
+            <Terminal></Terminal>
+          </div>
         </div>
       </div>
     </>
---
+++ src/components/terminal.jsx
@@ -0,0 +1,7 @@
+export default function Terminal() {
+  return (
+    <div>
+      <p>viewer@pc:~$ qsddqs</p>
+    </div>
+  );
+}
